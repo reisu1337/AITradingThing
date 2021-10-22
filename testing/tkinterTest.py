078b69cbe6b245aa327c95270559158ca9c65eff
reisu1337 <73112598+reisu1337@users.noreply.github.com>
2021-10-22 15:37:36 +0100

    added tkinterTest.py

---
+++ testing/tkinterTest.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+root = Tk()
+
+
+def main():
+    e = Entry(root, width=50, borderwidth=5)
+    e.grid(row=0, column=0)
+    e.insert(0, "Enter your name")
+
+    def myClick():
+        label = Label(root, text=e.get())
+        label.grid(row=2, column =0)
+
+    button = Button(root, text="Click the button!", command=myClick)
+    button.grid(row = 1, column=0)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
