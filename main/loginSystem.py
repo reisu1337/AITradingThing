4933db75dd5ad12f92a47925facd11ba618e476d
reisu1337 <73112598+reisu1337@users.noreply.github.com>
2022-01-16 14:50:37 +0000

    Created a function that checks if username exists in users db

--- main/loginSystem.py
+++ main/loginSystem.py
@@ -1,1 +1,20 @@
+import database
+
+
+def checkUsername(username):
+    # Returns False if username is already in database, and True if not
+    con, cur = database.connectDataBase()
+    checkStatement = "SELECT COUNT(*) FROM users WHERE username =?"
+    check = (cur.execute(checkStatement, (username,))).fetchone()[0]
+    if check >= 1:
+        return False
+    else:
+        return True
+
+
 def registerUser(username, password, confpassword):
+    return True
+
+
+if __name__ == "__main__":
+    print(checkUsername("Test"))
