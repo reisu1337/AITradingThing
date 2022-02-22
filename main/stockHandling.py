c5f34e7e843d1f7dc90b4b6d7e53eaf25d17313f
reisu1337 <73112598+reisu1337@users.noreply.github.com>
2022-02-22 13:38:36 +0000

    Data is retrieved and returned, and an error is returned if the stock doesn't exist

--- main/stockHandling.py
+++ main/stockHandling.py
@@ -1,7 +1,4 @@
-import pandas as pd
-import plotly.graph_objs as go
 import yfinance as yf
-import plotly.express as px
 import pandas
 import os
 from os.path import exists
@@ -11,18 +8,23 @@ from datetime import datetime
 def collectData(ticker):
     today = datetime.today()
     d4 = today.strftime("%b-%d-%Y")
-    data = yf.download(tickers=ticker, period="1y", interval="1d", rounding=True)
-    path = os.path.join(os.path.dirname(__file__), f"{d4+ticker}.csv")
+    data = yf.download(tickers=ticker, period="1y", interval="1d", rounding=True, progress=False, show_errors=False)
+    if data.empty:
+        return False
+    path = os.path.join(os.path.dirname(__file__), f"{d4 + ticker}.csv")
     pandas.DataFrame.to_csv(data, path)
+    return True
 
-def createGraph(ticker):
+
+def retrieveData(ticker):
     today = datetime.today()
     d4 = today.strftime("%b-%d-%Y")
-    if not exists(os.path.dirname(__file__)+"\\"+d4+ticker+".csv"):
-        collectData(ticker)
-    df = pandas.read_csv(os.path.dirname(__file__)+"\\"+d4+ticker+".csv")
-    print(df)
+    if not exists(os.path.dirname(__file__) + "\\" + d4 + ticker + ".csv"):
+        if not collectData(ticker):
+            return "Stock Not Retrieved"
+    df = pandas.read_csv(os.path.dirname(__file__) + "\\" + d4 + ticker + ".csv")
+    return df
 
 
 if __name__ == "__main__":
-    createGraph("GOOGL")
+    print(retrieveData("GOOGL"))
